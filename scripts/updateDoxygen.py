b2c1a0788787f589bfd165d435c5d2c22541f2e2
zimmer <zimmer@fysik.su.se>
2016-04-08 15:19:50 +0200

    fix.

--- scripts/updateDoxygen.py
+++ scripts/updateDoxygen.py
@@ -153,7 +153,7 @@ if __name__ == '__main__':
         run(['tar xzvf doxygen.tar.gz'])
         os.remove('doxygen.tar.gz')
     os.chdir(os.path.join(out_dir,"Documentation"))
-    run(["(cat do.config; echo \"PROJECT_NUMBER=%s\") | %s -"%("Trunk" if opts.release else opts.tag.strip("DmpSoftware-"),
+    run(["(cat do.config; echo \"PROJECT_NUMBER=%s\") | %s -"%("Trunk" if opts.release else opts.svn_tag.strip("DmpSoftware-"),
                                                                cfg['doxygen_binary'])])
     # next cleanup
     if not opts.skip_cleanup:
