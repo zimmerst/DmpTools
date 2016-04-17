98e32cc2154f52a155a7c775723391765febdb53
zimmer <zimmer@fysik.su.se>
2016-04-17 15:02:29 +0200

    adding CERN-version of indico crawler.

--- scripts/fetchIndicoEventCERN.py
+++ scripts/fetchIndicoEventCERN.py
@@ -1,14 +1,14 @@
 """
 @author: S. Zimmer (Geneva)
-@brief: convenience script to crawl Indico page and extract all contributions for each event with identical names
+@brief: convenience script to crawl CERN Indico page and extract all contributions for each event with identical names
 """
 
-
 import time, datetime, json, urllib, hmac, hashlib, sys
-from __builtin__ import None
+# CERN needs both api-key & api-secret (only persistent requests are setup)
+GLOB_API_KEY = ''
+GLOB_API_SECRET = ''
+GLOB_HOSTNAME= "https://indico.cern.ch"
 
-GLOB_API_KEY = 'enter here API key from Indico'
-GLOB_HOSTNAME= 'http://address.of.host'
 
 def contrib2twiki(cdict):
 	ret = "   * *%s* - %s"%(cdict['title'],cdict['speaker'])
@@ -37,32 +37,41 @@ class IndicoObject(object):
 	PATH= None
 	ID = None
 	API_KEY = GLOB_API_KEY
+	API_SECRET = GLOB_API_SECRET
 	PARAMS = {}
 	URL = None
 	JSON = None
+
 	def __buildIndicoRequest__(self):
-		items = self.PARAMS.items()
+		items = []
+		items += self.PARAMS.items()
 		items.append(('ak',self.API_KEY))
 		items = sorted(items, key=lambda x: x[0].lower())
 		path = '%s/%s.json'%(self.PATH,self.ID)
-		self.URL = '%s?%s' % (path, urllib.urlencode(items))
+		merged_path = '%s?%s' % (path, urllib.urlencode(items))
+		signature = hmac.new(self.API_SECRET, merged_path, hashlib.sha1).hexdigest()
+    # remove 0 item
+		del items[0]
+		items = sorted(items, key=lambda x: x[0].lower())
+		items.append(('ak',self.API_KEY))
+		merged_path = '%s?%s' % (path, urllib.urlencode(items))
+		self.URL = "%s%s&signature=%s"%(GLOB_HOSTNAME,merged_path,signature)
 		return self.URL
-
+	
 	def __init__(self,**kwargs):
 		self.__dict__.update(kwargs)
 		self.__setDefaults__()
 		self.__buildIndicoRequest__()
-
+	
 	def __setDefaults__(self):
 		self.PARAMS['limit']='123'
 		self.PARAMS['pretty']='yes'
-
+	
 	def setObjectId(self,EVENT_ID):
 		self.ID=EVENT_ID
 	
 	def validateJSON(self):
 		if self.JSON is None:							return IS_OK(False,'JSON query empty, maybe something went wrong')
-		if not self.JSON['complete']:			return IS_OK(False,'JSON query not complete')
 		if not len(self.JSON['results']): return IS_OK(False,'JSON query seemingly okay, but results are empty')
 		content = self.JSON['results']
 		return IS_OK(True,"Everything is Fine")
@@ -77,9 +86,9 @@ class IndicoObject(object):
 		return True
 
 class IndicoCategory(IndicoObject):
-	PATH = "%s/export/categ"%GLOB_HOSTNAME
+	PATH = "/export/categ"
 	PARAMS ={'detail':'events'}
-
+	
 	def getEvents(self,title):
 		ret = []
 		res = self.validateJSON()
@@ -93,9 +102,9 @@ class IndicoCategory(IndicoObject):
 		return ret
 
 class IndicoEvent(IndicoObject):
-	PATH = "%s/export/event"%GLOB_HOSTNAME
+	PATH = "/export/event"
 	PARAMS = {'detail':'contributions'}
-
+	
 	def getContributions(self,with_material=True):
 		''' returns the contributions as list of dictionaries '''
 		ret = []
@@ -126,9 +135,9 @@ class IndicoEvent(IndicoObject):
 					continue
 				r['slides']=mat[0]['resources'][0]['url']
 			ret.append(r)
-			#print ret[-1]
+		#print ret[-1]
 		return sorted(ret, key = lambda user: user['timestamp'])
-
+	
 	def getMinutes(self):
 		ret = None
 		res = self.validateJSON()
@@ -136,13 +145,12 @@ class IndicoEvent(IndicoObject):
 			print 'error validating JSON, returning an empty list, error below'
 			print	res['Message']
 			return ret
-		material = self.JSON['results'][-1]['material']
-		if len(material):
-			for m in material:
-				if m['id']=='minutes': ret = m['resources'][-1]['url']
+		material = self.JSON['results'][-1]['material'][0] # will always be a list
+		if 'id' in material:
+			if m['id']=='minutes': ret = material['resources'][-1]['url']
 		return ret
-	
-	
+
+
 if __name__ == '__main__':
 	from optparse import OptionParser
 	parser = OptionParser()
@@ -150,11 +158,11 @@ if __name__ == '__main__':
 	description = "fetch Indico content from %s"%GLOB_HOSTNAME
 	parser.set_usage(usage)
 	parser.set_description(description)
-	parser.add_option("--title",dest='title',default='Simulation Group Meetings',help="title to be used in twiki")
-	parser.add_option("--meeting_title",dest='mtitle',default="DAMPESW Simulation ",help="title of event in Indico")
+	parser.add_option("--title",dest='title',default='DAMPE-EU data analysis',help="title to be used in twiki")
+	parser.add_option("--meeting_title",dest='mtitle',default="DAMPE data analysis",help="title of event in Indico")
 	(opts, arguments) = parser.parse_args()
 	doc_header = "---+ DAMPE %s\n*Do not edit this page manually, as it is created by a bot*"%opts.title
-	my_cat = IndicoCategory(ID=4)
+	my_cat = IndicoCategory(ID=7913)
 	my_cat.submitQuery()
 	events = reversed(my_cat.getEvents(title=opts.mtitle))
 	print doc_header
@@ -163,6 +171,7 @@ if __name__ == '__main__':
 		h = "---++++ [[%s][%s]]"%(ev['url']," ".join([startDate[key] for key in ['date','time','tz']]))
 		if i == 0 and nextMeeting(startDate): h+= " *(next meeting)*"
 		print h
+		#print ev['title']
 		my_event = IndicoEvent(ID=int(ev['id']))
 		my_event.submitQuery()
 		contribs = my_event.getContributions()
@@ -171,4 +180,5 @@ if __name__ == '__main__':
 		minutes = my_event.getMinutes()
 		if minutes: print "   * [[%s][Minutes]]"%minutes
 	print "\nLast Update: %s"%time.ctime()
+
 
