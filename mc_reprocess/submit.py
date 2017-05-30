09d025cc99783c3f9b0c174a8bf0aea2997cb509
zimmer <zimmer@fysik.su.se>
2017-05-30 20:21:56 +0200

    fix

--- mc_reprocess/submit.py
+++ mc_reprocess/submit.py
@@ -50,7 +50,7 @@ for i in xrange(ncycles):
     files_to_process = []
     for t in txtfiles:
         print 'reading %s...'%t
-        files_to_process+=[f.replace("\n","") for f in open(t,"r").read()]
+        files_to_process+=[f.replace("\n","") for f in open(t,"r").readlines()]
         print 'size: ',len(files_to_process)
     wd=opjoin(cfg['workdir'],cfg['tag'])
     wd=opjoin(wd,"cycle_%i"%(i+1))
