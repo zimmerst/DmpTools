084a2302c732bc113fae5425e3c688441c568372
zimmer <zimmer@fysik.su.se>
2017-05-30 23:20:44 +0200

    fixes.

--- mc_reprocess/submit_slurm.sh
+++ mc_reprocess/submit_slurm.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-export WORKDIR=${WORKDIR}
-export SWPATH=${SWPATH}
-export SCRATCH=${SCRATCH}
 ###### below is SLURM specific stuff #######
 #SBATCH --output=${WORKDIR}/%A_%a.out
-#SBATCH --output=${WORKDIR}/%A_%a.err
+#SBATCH --error=${WORKDIR}/%A_%a.err
 #SBATCH --partition=mono-shared
 #SBATCH --time=${STIME}
 #SBATCH --mem=${SMEM}
 #SBATCH --array=${SARR}
+export WORKDIR=${WORKDIR}
+export SWPATH=${SWPATH}
+export SCRATCH=${SCRATCH}
 #SBATCH --export=SWPATH
 #SBATCH --export=WORKDIR
 #SBATCH --export=SCRATCH
