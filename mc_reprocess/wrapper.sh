7e60363a88cae860a3cab2116fe61844313ff76b
zimmer <zimmer@fysik.su.se>
2017-05-30 23:22:35 +0200

    fixes.

--- mc_reprocess/wrapper.sh
+++ mc_reprocess/wrapper.sh
@@ -13,6 +13,6 @@ cd ${SWPATH}/bin
 source thisdmpsw.sh
 cd ${SCRATCH}
 ## EXECUTABLE BELOW ##
-exe="python ${DMPSWSYS}/share/TestRelease/JobOption_MC_DigiReco_Prod.py -y ${WORKDIR}/cycle_${SLURM_ARRAY_JOB_ID}.yaml"
+exe="python ${DMPSWSYS}/share/TestRelease/JobOption_MC_DigiReco_Prod.py -y ${WORKDIR}/cycle_${SLURM_ARRAY_TASK_ID}.yaml"
 
 echo ${exe}
