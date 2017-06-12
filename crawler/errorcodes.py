0e72ffe018a5f747da28913ab6953327d9c55223
David Francois Droz <david.droz@outlook.com>
2017-06-12 16:56:11 +0200

    Cleaned up code

--- crawler/errorcodes.py
+++ crawler/errorcodes.py
@@ -22,19 +22,16 @@ Error codes:
 
 '''
 
-
 import json
 import os
 import sys
 
 def json_load_byteified(file_handle):
+	'''
+	Crawler output is unicode. Change to UTF-8 for better handling
+	'''
     return _byteify(
         json.load(file_handle, object_hook=_byteify),
-        ignore_dicts=True
-    )
-def json_loads_byteified(json_text):
-    return _byteify(
-        json.loads(json_text, object_hook=_byteify),
         ignore_dicts=True
     )
 def _byteify(data, ignore_dicts = False):
@@ -49,156 +46,98 @@ def _byteify(data, ignore_dicts = False):
         }
     return data
 
+def identifyEnergyRange(filename):
+	'''
+	Identifies energy range by looking at the name of the file (not at the event header)
+	'''
+	if "10TeV_100TeV" in filename:
+		energyMin = 1e+7
+		energyMax = 1e+8
+	elif "10GeV_100GeV" in filename:
+		energyMin = 1e+4
+		energyMax = 1e+5
+	elif "100GeV_10TeV" in filename:
+		energyMin = 1e+5
+		energyMax = 1e+7
+	elif "1GeV_100GeV" in filename:
+		energyMin = 1e+3
+		energyMax = 1e+5
+	else:
+		raise Exception("Energy range not recognised")
+	return energyMin, energyMax
 
-def _ana(filename,boolwrite=False):
+
+def ana(filename):
 	if not '.json' in filename:
 		raise IOError('Not a .json file')
 	
 	with open(filename,'r') as f:	
 		diclist = json_load_byteified(f)
 	
-	
-	# Identify energy range:
-	if "10TeV_100TeV" in diclist[0]['lfn']:
-		energyMin = 1e+7
-	elif "10GeV_100GeV" in diclist[0]['lfn']:
-		energyMin = 1e+4
-	elif "100GeV_10TeV" in diclist[0]['lfn']:
-		energyMin = 1e+5
-	elif "1GeV_100GeV" in diclist[0]['lfn']:
-		energyMin = 1e+3
-	else:
-		raise Exception("Energy range not recognised")
-	
-	
-	fichiers = {'0':[],'1001':[],'1002':[],'1003':[],'1004':[],'2000':[],'1005':[],'3000':[]}
-	tailles = []
-	for i in range(8):
-		tailles.append([])
+	eMin,eMax = identifyEnergyRange(diclist[0]['lfn'])
 	emins = []
 	emaxs = []
 	
+	# Explicitly have one list per possible error code - at the end I want empty files for error codes that did not appear (instead of having no file at all)
+	fichiers = {'0':[],'1001':[],'1002':[],'1003':[],'1004':[],'2000':[],'1005':[],'3000':[]}
+
 	for iteration in diclist:
-		
-		# List error codes
-		if iteration['error_code'] == 0:
-			fichiers['0'].append(iteration['lfn'])
-			tailles[0].append(iteration['nevts'])
-		elif iteration['error_code'] == 1001:
-			fichiers['1001'].append(iteration['lfn'])
-			tailles[1].append(iteration['nevts'])
-		elif iteration['error_code'] == 1002:
-			fichiers['1002'].append(iteration['lfn'])
-			tailles[2].append(iteration['nevts'])
-		elif iteration['error_code'] == 1003:
-			fichiers['1003'].append(iteration['lfn'])
-			tailles[3].append(iteration['nevts'])
-		elif iteration['error_code'] == 1004:
-			fichiers['1004'].append(iteration['lfn'])
-			tailles[4].append(iteration['nevts'])
-		elif iteration['error_code'] == 2000:
-			fichiers['2000'].append(iteration['lfn'])
-			tailles[5].append(iteration['nevts'])
-		elif iteration['error_code'] == 1005:
-			fichiers['1005'].append(iteration['lfn'])
-			tailles[6].append(iteration['nevts'])
-		elif iteration['error_code'] == 3000:
-			fichiers['3000'].append(iteration['lfn'])
-			tailles[7].append(iteration['nevts'])
-		else:
-			raise Exception('Unmanaged error code:' + str(iteration['error_code']))
-			
-		# Check for energy range
+		fichiers[str(iteration['error_code'])].append(iteration['lfn'])
 		if iteration['error_code'] == 0:
 			emins.append(iteration['emin'])
 			emaxs.append(iteration['emax'])
 			
 	if len(set(emins)) > 1 or len(set(emaxs)) > 1:		# Multiple energy ranges found
-		print "Energies found, lower bound: ", list(set(emins))
-		print "Energies found, upper bound: ", list(set(emaxs))
 		print "Found multiple energy ranges! File: ", filename.replace('.json','')
 		badEnergies = True
-	elif energyMin not in emins: 		# Wrong energy range
+	elif eMin not in emins or eMax not in emaxs: 		# Wrong energy range
 		badEnergies = True
 		print "Found bad energy range! File: ", filename.replace('.json','')
 	else:
 		badEnergies = False
 	
-	# Write results		
-	if boolwrite:
-		for blarg in fichiers.keys():
+	# Write results	
+	dirname = './ana/' + os.path.splitext(os.path.basename(filename))[0]
+	for d in ['ana',dirname]
+		if not os.path.isdir(d): os.mkdir(d)
+		
+	for k in fichiers.keys():
+		outstring = dirname + '/error' + k + '.txt'
+		with open(outstring,'w') as thefile:
+			for item in fichiers[k]:
+				thefile.write("%s\n" % item)
+	if badEnergies:
+		if not os.path.isdir(dirname+'/energies'): os.mkdir(dirname+'/energies')
+		
+		dicEnergy = {}
+		for x in list(set(emins)):
+			dicEnergy[str(x)] = []
+		for iteration in diclist:
+			if iteration['error_code'] == 0:
+				dicEnergy[str(iteration['emin'])].append(iteration['lfn'])
+		
+		for key in dicEnergy.keys():
+			if float(key) == 1e+4: erange='10GeV_100GeV.txt'
+			elif float(key) == 1e+5: erange='100GeV_10TeV.txt'
+			elif float(key) == 1e+7: erange='10TeV_100TeV.txt'
+			elif float(key) == 1e+3: erange='1GeV_100GeV.txt'
+			else: raise Exception("Energy range not recognised!")
+			outstring = dirname + '/energies/' + erange
 			
-			if not os.path.isdir('outputs'):
-				os.mkdir('outputs')
-			
-			dirname = './outputs/' + os.path.splitext(os.path.basename(filename))[0]
-			if not os.path.isdir(dirname):
-				os.mkdir(dirname)
-			outstring = dirname + '/error' + blarg + '.txt'
-				
 			with open(outstring,'w') as thefile:
-				for item in fichiers[blarg]:
+				for item in dicEnergy[key]:
 					thefile.write("%s\n" % item)
-		if badEnergies:
-			if not os.path.isdir(dirname+'/energies'): os.mkdir(dirname+'/energies')
-			
-			dicEnergy = {}
-			for x in list(set(emins)):
-				dicEnergy[str(x)] = []
-			for iteration in diclist:
-				if iteration['error_code'] == 0:
-					dicEnergy[str(iteration['emin'])].append(iteration['lfn'])
-			
-			for key in dicEnergy.keys():
-				if float(key) == 1e+4: erange='10GeV_100GeV.txt'
-				elif float(key) == 1e+5: erange='100GeV_10TeV.txt'
-				elif float(key) == 1e+7: erange='10TeV_100TeV.txt'
-				elif float(key) == 1e+3: erange='1GeV_100GeV.txt'
-				else: raise Exception("Energy range not recognised!")
-				outstring = dirname + '/energies/' + erange
-				
-				with open(outstring,'w') as thefile:
-					for item in dicEnergy[key]:
-						thefile.write("%s\n" % item)
-				
-	return fichiers, tailles
-	
+
 
 if __name__ == '__main__':
 	
 	if ".json" in sys.argv[1]:		# Only one json file provided
-		filecount, filesize = _ana(sys.argv[1],boolwrite=True)
-		
+		ana(sys.argv[1],boolwrite=True)
 	else:							# ASCII list of json files provided
 		inputlist = []
 		with open(sys.argv[1],'r') as f:
 			for lines in f:
-				inputlist.append(lines.replace('\n',''))
-		
-			filecounts = []
-			filesizes = []
-			totals = {'0':[],'1001':[],'1002':[],'1003':[],'1004':[],'2000':[],'1005':[],'3000':[]}
-		
-		# Loop on .json files	
+				inputlist.append(lines.replace('\n',''))		
 		for fichier in inputlist:
-			
-			cnt, sze = _ana(fichier)
-			filecounts.append(cnt)
-			filesizes.append(sze)
-			
-			for k in totals.keys() :
-				totals[k] = totals[k] + cnt[k]
-		
-		# Loop over, writing to file		
-		for k in totals.keys() :
-			outname = './out_ana/out_err' + k + '_' + sys.argv[1]
-			with open(outname,'w') as f:
-				for item in totals[k]:
-					f.write("%s\n" % item)
-		print '----------------------------------------------'
-		print ' --- TOTAL --- '
-		a = 0
-		for k in totals.keys():
-			a = a + len(totals[k])
-			print k, " : ", len(totals[k])
-		print "Total : ", a
+			ana(fichier)
+
