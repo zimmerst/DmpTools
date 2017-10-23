ee197c77b674f7375e28d593141c08901626cabb
David Francois Droz <david.droz@outlook.com>
2017-10-23 11:05:05 +0200

    Insignificant syntax improvement

--- crawler/errorcodes.py
+++ crawler/errorcodes.py
@@ -33,21 +33,21 @@ def json_load_byteified(file_handle):
 	'''
 	Crawler output is unicode. Change to UTF-8 for better handling
 	'''
+	def _byteify(data, ignore_dicts = False):
+		if isinstance(data, unicode):
+			return data.encode('utf-8')
+		if isinstance(data, list):
+			return [ _byteify(item, ignore_dicts=True) for item in data ]
+		if isinstance(data, dict) and not ignore_dicts:
+			return {
+				_byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
+				for key, value in data.iteritems()
+				}
+		return data
 	return _byteify(
 		json.load(file_handle, object_hook=_byteify),
 		ignore_dicts=True
 	)
-def _byteify(data, ignore_dicts = False):
-	if isinstance(data, unicode):
-		return data.encode('utf-8')
-	if isinstance(data, list):
-		return [ _byteify(item, ignore_dicts=True) for item in data ]
-	if isinstance(data, dict) and not ignore_dicts:
-		return {
-			_byteify(key, ignore_dicts=True): _byteify(value, ignore_dicts=True)
-			for key, value in data.iteritems()
-			}
-	return data
 
 def identifyEnergyRange(filenamedir):
 	'''
@@ -153,8 +153,6 @@ def ana(filename):
 			with open(outstring,'w') as thefile:
 				for item in dicEnergy[key]:
 					thefile.write("%s\n" % item)
-	
-	
 	
 
 if __name__ == '__main__':
