2393660c90be4fe72094fa44992a309964860823
zimmer <zimmer@fysik.su.se>
2017-12-04 23:02:30 +0100

    no need for this import.

--- HE_skimmer/code/scripts/read_local_file.C
+++ HE_skimmer/code/scripts/read_local_file.C
@@ -1,5 +1,5 @@
 void read_local_file(TString filename){
-  gSystem->Load("libDmpEvent.so");
+  //gSystem->Load("libDmpEvent.so");
   if (filename.Contains("dpm"))
     TFile *file = TFile::Open("grid05.unige.ch:1094"+filename);
   else
